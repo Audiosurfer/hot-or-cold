ddbbf9d0b650643c0d449a4a23d77b9c8e46f834
Audiosurfer <gabolad@hotmail.com>
2014-09-16 18:27:40 -0500

    code refactored & now gives feedback on guesses
    when you input a guess, it will tell you how close you are to the
    correct answer. also, made the code cleaner.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var randNum;
     var guessAmount = 0;
     var guesses;
-    
+    var difference;
 	
     /*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -21,27 +21,77 @@ $(document).ready(function(){
         return randNum;
     }
     
+    /* Finds difference between guessed number and random number */
+    var guessDiff = function() {
+        difference = Math.abs(guesses - randNum);
+        return difference;
+    }
+    
     /* Add guesses to list of guesses */
     $('.button').click(function() {
         event.preventDefault();
-        guesses = $('#userGuess').val();
-        guessAmount += 1;
-        $('#count').text(guessAmount);
-        $('#guessList').append("<li>" + guesses + "</li>");
+        guessAdd();
+        guessCountAdd();
+        guessProx();
     })
     
+    /* Grabs value of guess from input field */
+    var guessGet = function() {
+        guesses = $('#userGuess').val();
+    }
+    
+    /* Increase guss count */
+    var guessCountAdd = function() {
+        guessAmount += 1;
+        $('#count').text(guessAmount);
+    }
+    
+    /* List guessed numbers */
+    var guessAdd = function() {
+        guessGet();
+        $('#guessList').append("<li>" + guesses + "</li>");
+    }
+    
+    /* Start a new game */
     var newGame = function() {
         $('#guessList li').hide();
         guessAmount = 0;
         $('#count').text(guessAmount);
+        $('#feedback').text("Make your Guess!");
         hiddenRandNum();
     }
     
+    /* Sees how close guessed value is to actual value */
+   var guessProx = function() {
+        guessGet();
+        guessDiff();
+        if (difference === 0) {
+            $('#feedback').text("Correct! " + guesses + " is the answer");
+        }
+        else if (difference <= 5) {
+             $('#feedback').text("You're burning up");
+        }
+        else if (difference <= 10) {
+             $('#feedback').text("You're red hot");
+        }
+        else if (difference <= 20) {
+             $('#feedback').text("You're hot");
+        }
+        else if (difference <= 30) {
+             $('#feedback').text("You're warm");
+        }
+        else if (difference <= 50) {
+             $('#feedback').text("You're cold");
+        }
+        else {
+            $('#feedback').text("You're ice cold");
+        }
+    } 
+     
     $('.new').click(function() {
         newGame();
     })
     newGame();
-    alert(randNum);
 });
 
 
